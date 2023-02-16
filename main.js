d3856f7b3d1d6af6ff1cb0292ecc9d185864a68d
Heldeg <edhernandezfo@unal.edu.co>
2023-02-16 15:45:16 -0500

    shop car menu

--- main.js
+++ main.js
@@ -2,16 +2,38 @@ const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu')
 const menuHamIcon = document.querySelector('.menu')
+const menuShopCartIcon = document.querySelector('.navbar-shopping-cart')
+const menuShopCart = document.querySelector('.product-detail');
 
-
-
-navEmail.addEventListener('click', toggleDesktopMenu); 
-menuHamIcon.addEventListener('click', toggleMobileMenu); 
+navEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuShopCartIcon.addEventListener('click', toggleShopCarMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isShopCarMenuClosed = menuShopCart.classList.contains('inactive');
+
+    if (isMobileMenuClosed && !isShopCarMenuClosed) {
+        toggleShopCarMenu();
+    }
+
     mobileMenu.classList.toggle('inactive');
+
+}
+
+function toggleShopCarMenu() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isShopCarMenuClosed = menuShopCart.classList.contains('inactive');
+
+    if (!isMobileMenuClosed && isShopCarMenuClosed) {
+        toggleMobileMenu();
+    }
+
+    menuShopCart.classList.toggle('inactive');
+
+
 }
